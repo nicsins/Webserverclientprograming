a050b3cf6cc703d9b5cb9c676be1009684a890c3
nic <DominicB@email.com>
2019-02-17 03:37:57 -0600

    lab4ist commit!

--- lab4/slide-practice/js/draw_word.js
+++ lab4/slide-practice/js/draw_word.js
@@ -1,4 +1,4 @@
-let canvas=document.querySelector("#canvas");
+let canvas=document.querySelector("#canvas2");
 let context=canvas.getContent("2d");
 
 let input=document.querySelector("#image-text");
@@ -10,12 +10,28 @@ image.src="js.buddypirate.png";
 
 image.addEventListener("load",function () {
     context.drawImage(image, 0, 0);
+    input.disabled = true;
+});
+
+
+let canvas2=document.querySelector("#canvas2");
+let context=canvas2.getContent("2d");
+
+let input2=document.querySelector("#image2-text");
+input.disabled=true;
+
+
+let image2=new Image();
+image2.src="js.buddypirate.png";
+
+image2.addEventListener("load",function () {
+    context.drawImage(image2, 0, 0);
     input.disabled = false;
 });
 input.addEventListener("input",function () {
     let text=this.value;
 
-    context.drawImage(image,0,0);
+    context.drawImage(image2,0,0);
     context.font("30 px Helvetica black");
     context.fillText(text,30,100);
 
